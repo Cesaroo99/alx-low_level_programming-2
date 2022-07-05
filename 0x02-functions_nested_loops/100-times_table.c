22b6f783ab92cc748b92e2c581032dda95cff40e
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-05 06:32:32 -0700

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_times_table - entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int nu, m, p;
+	if (n>= 0 && n <= 15)
+	{
+	for (nu = 0; nu <= n; nu++)
+	{
+	_putchar('0');
+	for (m = 1; m <= n; m++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	p = nu * m;
+	if (p <= 99)
+		_putchar(' ');
+	if (p <= 9)
+		_putchar(' ');
+	if (p >= 100)
+	{
+	_putchar((p / 100) + '0');
+	_putchar(((p / 10) % 10 ) + '0');
+	}
+	else if (p <= 99 && p >= 10)
+	{
+	_putchar((prod % 10) + '0');
+	}
+	_putchar((p % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
+}
