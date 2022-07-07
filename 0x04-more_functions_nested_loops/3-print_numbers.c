792b7cf5c0554f4790dacc56466d9e1e0a2afa2e
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-07 10:26:43 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,15 @@
 /**
  * print_numbers - checks for a digit
  *
- * Return: 0
+ * Return: Always 0
  */
 
 void print_numbers(void)
 {
-int a;
 
-for (a = 48; a > 58; a++)
+	int a;
+
+for (a = 48; a < 58; a++)
 {
 _putchar(a);
 }
