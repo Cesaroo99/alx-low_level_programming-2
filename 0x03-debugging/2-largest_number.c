44d212a25ef351ab95f58a41fabd1a92128b9b5e
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-07 03:17:39 -0700

    task 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first number
+ * @b: second
+ * @c: third
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+int l;
+
+if (a > b && b > c)
+	l = a;
+else if (a > b && b < c)
+{
+if (a > c)
+	l =a;
+else
+	l = c;
+}
+else if (b > a && a > c)
+	l = b;
+	else if (b > a && a < c)
+{
+if (b > c)
+	l = b;
+else
+	l = c;
+}
+else if (a == b && a == c)
+	l = a;
+	else
+{
+if ((a == b && a > c) || (a == c && a > b) || (b == c && a > b))
+	l = a;
+else if ((a == c && a < b) || (b == c && a < b))
+	l = b;
+else
+	l = c;
+}
+return (l);
+}
