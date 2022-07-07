f037d6385b9d538251adddf990d1b043fbf51e16
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-07 10:07:36 -0700

    task 1 fixed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void print_to_98(int n);
 int _isupper(int c);
-int isdigit(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
