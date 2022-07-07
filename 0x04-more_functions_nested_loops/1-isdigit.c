d00a1e5d264dc8d8d67fa799552d9b14104b6f9f
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-07 09:01:24 -0700

    mandatory task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for checks for a digit
+ * @c: variable
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - checks for a digit
+ * @a: a - variable
+ * @b: b - variable
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
