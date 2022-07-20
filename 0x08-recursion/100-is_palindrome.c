cbddcd861f1d549e146fc212d317e476b4388539
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-20 10:12:58 -0700

    task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * p1 - obtains the length
+ * @a: string
+ * @l: integer
+ *
+ * Return: on success 1
+ * on error -1
+ */
+int p1(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (p1(a + 1, l + 1));
+}
+/**
+ * p2 - compares string
+ * @a: string
+ * @l: length
+ *
+ * Return: on success 1
+ * on error -1
+ */
+
+int p2(char *a, int l)
+{
+	if (*a != *(a + 1))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (p2(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: on success 1
+ * on error -1
+ */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = p1(s, 0);
+	return (p2(s, l));
+}
