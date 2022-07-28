68552649b4a990ebad413dde3bfbe518850007e2
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-07-28 08:39:42 -0700

    task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,20 +1,20 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-
-	/**
-	 * _realloc - allocates memory block using malloc free
-	 * @ptr: pointer
-	 * @old_size: the size 1
-	 * @new_size: size 2
-	 *
-	 * Description: allocates a new memory block for the pointer
-	 *
-	 * Return: A pointer
-	 * NULL if can not allocate memory
-	 */
-	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+/**
+ * _realloc - allocates memory block using malloc free
+ * @ptr: pointer
+ * @old_size: the size 1
+ * @new_size: size 2
+ *
+ * Description: allocates a new memory block for the pointer
+ *
+ * Return: A pointer
+ * NULL if can not allocate memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i;
 	char *p;
 
 	if (new_size == old_size)
@@ -26,34 +26,18 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		return (NULL);
 	}
 
-	p = malloc(new_size);
+	p = malloc(sizeof(char) * new_size);
 
 	if (p == NULL)
 		return (NULL);
 
-	if (ptr == NULL)
-		return (p);
-
-	p = _memcpy(p, ptr, (new_size > old_size ? old_size : new_size));
+	if (ptr)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			p[i] = *((char *)ptr + i);
+		}
+	}
 	free(ptr);
 	return (p);
-}
-
-/**
- * _memcpy - copies the memory
- * @dest: the destination
- * @src: the source pointer
- *
- * Return: The pointer
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	return (dest);
 }
