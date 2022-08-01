7c6adea579f896a580b9c34857e9927e4b6a06a6
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-08-01 04:38:57 -0700

    task dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_
-#define _DOG_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog _ dog with a few attributes
@@ -7,16 +7,16 @@
  * @age: the age
  * @owner: the name of the owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-char *dup_strint(char *s);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
