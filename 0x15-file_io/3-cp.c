df4f56997f1f47f524fa70b898940b2a7ac5ab78
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-08-22 08:23:38 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	}
 	if (numb == -1)
 	{
-		dprinntf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if ((close(file_origin) == -1) || (close(file_dest) == -1))
