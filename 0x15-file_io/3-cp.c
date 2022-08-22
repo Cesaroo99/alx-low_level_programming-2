95e33d066af0c8273294e6b140f533c253c45321
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2022-08-22 07:12:06 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,25 +35,25 @@ int main(int argc, char *argv[])
 	file_dest = open(argv[2], O_CREAT | O_WRONLY, 0664);
 	if (file_dest == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while((numb = read(file_origin, buff, BUFF_SIZE)) > 0)
+	while ((numb = read(file_origin, buff, BUFF_SIZE)) > 0)
 	{
 		if (write(file_dest, buff, numb) != numb)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprint(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (numb == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if ((close(file_origin) == -1) || (close(file_dest) == -1))
 	{
-		dprintf(STDERR_FILENO, "Error: Cant't close fd %d\n", file_origin);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_origin);
 		exit(100);
 	}
 	exit(EXIT_SUCCESS);
